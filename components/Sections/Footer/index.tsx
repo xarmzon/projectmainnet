1698b85cb2464ddbd096eb8e2bbbe59b55debde9
unknown <xarmzon@gmail.com>
2022-03-13 18:58:39 +0000

    update to site3

--- components/Sections/Footer/index.tsx
+++ components/Sections/Footer/index.tsx
@@ -5,7 +5,7 @@ import { APP_NAME } from '../../../utils/constants'
 const Footer = () => {
   return (
     <div className="from-secondary to-secondary/40 flex h-20 flex-col items-center justify-center border-t-[1px] border-t-slate-700/30 bg-gradient-to-t">
-      <div className="mb-1 flex items-center space-x-2 text-primary-green transition-all duration-500 hover:text-primary-green/70">
+      <div className="mb-1 flex items-center space-x-2 text-primary-green transition-all duration-500 hover:text-primary-green/70 md:text-xl lg:text-2xl">
         <FaTwitter />
         <FaInstagram />
         <FaYoutube />
