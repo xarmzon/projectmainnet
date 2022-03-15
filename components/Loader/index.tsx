08bf636bfa83d581c75b2b4671b8f57d018e84b0
unknown <xarmzon@gmail.com>
2022-03-15 02:49:09 +0000

    update loader background

--- components/Loader/index.tsx
+++ components/Loader/index.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className="h-30 w-30 flex flex-col items-center justify-center space-y-4">
-      <span className="pointer-events-none block h-10 w-10 animate-spin rounded-xl bg-gradient-to-bl from-accent to-primary text-center"></span>
-      <p className=" text-sm capitalize text-primary">processing...</p>
+      <span className="pointer-events-none block h-10 w-10 animate-spin rounded-xl bg-linearGreen2"></span>
+      <p className=" text-primary text-sm capitalize">processing...</p>
     </div>
   )
 }
